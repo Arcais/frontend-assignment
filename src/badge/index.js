84bf38f5761db947d91707f49b10427fa24fda8b
Arcais <dragos.petre.paun@gmail.com>
2020-10-21 20:14:59 +0200

    Added basic badge and tooltip icon

--- src/badge/index.js
+++ src/badge/index.js
@@ -1,10 +1,19 @@
 import { h } from 'preact';
 import { css } from 'goober';
 
+const BadgeClass = ({color, bgColor: backgroundColor}) => css({
+	color,
+	backgroundColor
+});
 
-// TODO implement badge here
-const Badge = (props) => {
-	console.log(props);
+// props: {text, tooltip, bgColor, color}
+const Badge = ({text, color, bgColor}) => {
+	return (
+		<div class={ BadgeClass({ color, bgColor }) }>
+			{text}
+			<img src="img/icon_tooltip.svg" alt="Tooltip Icon" />
+		</div>
+	);
 }
 
 export default Badge;
